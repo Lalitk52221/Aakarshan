9c832a02894f0d603dac12b5fb67f0e3f43432bb
Lalit kumar <139781963+Lalitk52221@users.noreply.github.com>
2025-04-22 22:24:34 +0530

    add auth page

---
+++ src/app/sign-in/page.tsx
@@ -0,0 +1,141 @@
+"use client";
+// Shadcn UI
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
+
+// react-icons
+
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import Link from "next/link";
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { TriangleAlert } from "lucide-react";
+
+const SignIn = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [pending, setPending] = useState(false);
+  const router = useRouter();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setPending(true);
+    const res = await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
+    if (res?.ok) {
+      router.push("/");
+      toast.success("Login Successfull");
+    } else if (res?.status === 401) {
+      setError("Invalid credentials");
+      setPending(false);
+    } else {
+      setError("Something went wrong");
+    }
+  };
+  const handleProvider = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    value: "github" | "google"
+  ) => {
+    event.preventDefault();
+    signIn(value, { callbackUrl: "/" });
+  };
+  return (
+    <div className="h-full flex items-center justify-center bg-[#1b0918] ">
+      <Card className="md:h-auto w-[80%] sm:w-[420px] p-4 sm:p-8 ">
+        <CardHeader>
+          <CardTitle className="text-center">Sign In</CardTitle>
+          <CardDescription className="text-sm text-center text-accent-foreground">
+            Use Email and Services to sign in
+          </CardDescription>
+        </CardHeader>
+        {!!error && (
+          <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6 ">
+            <TriangleAlert />
+            <p>{error}</p>
+          </div>
+        )}
+        <CardContent className="px-2 sm:px-6 ">
+          <form onSubmit={handleSubmit} className="space-y-3">
+            <Input
+              type="email"
+              disabled={pending}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              placeholder="Email"
+            />
+            <Input
+              type="password"
+              disabled={pending}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Password"
+            />
+
+            <Button
+              className="w-full"
+              size={"lg"}
+              variant="default"
+              type="submit"
+              disabled={pending}
+            >
+              Continue
+            </Button>
+            <Separator />
+            <div className="flex my-2 justify-evenly mx-auto items-center">
+              <Button
+                disabled={false}
+                onClick={(e) => handleProvider(e, "google")}
+                variant="outline"
+                size="lg"
+                className="bg-slite-300 hover:bg-slate-400 hover:scale-110 "
+              >
+                <FcGoogle className="size-8 left-2.5 top-2.5" />
+              </Button>
+              <Button
+                disabled={false}
+                onClick={(e) => handleProvider(e, "github")}
+                variant="outline"
+                size="lg"
+                className="bg-slite-300 hover:bg-slate-400 hover:scale-110 "
+              >
+                <FaGithub className="size-8 left-2.5 top-2.5" />
+              </Button>
+            </div>
+            <p className="text-center text-sm mt-2 text-muted-foreground">
+              Create a new account?
+              <Link
+                href="sign-up"
+                className="text-sky-700 hover:underline  cursor-pointer"
+              >
+                {" "}
+                Sign Up
+              </Link>
+            </p>
+          </form>
+        </CardContent>
+      </Card>
+      Sign Up
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/app/sign-up/page.tsx
@@ -0,0 +1,169 @@
+"use client";
+// Shadcn UI
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Separator } from "@/components/ui/separator";
+import { Input } from "@/components/ui/input";
+
+// react-icons
+
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import Link from "next/link";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { TriangleAlert } from "lucide-react";
+import { signIn } from "next-auth/react";
+
+const SignUp = () => {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState(null);
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setPending(true);
+    const res = await fetch("/api/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    });
+    const data = await res.json();
+    if (res.ok) {
+      setPending(false);
+
+      toast.success(data.message);
+      router.push("/sign-in");
+    } else if (res.status == 400) {
+      setPending(false);
+      setError(data.message);
+    } else if (res.status == 500) {
+      setPending(false);
+      setError(data.message);
+    }
+  };
+ const handleProvider = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    value: "github" | "google"
+  ) => {
+    event.preventDefault();
+    signIn(value, { callbackUrl: "/" });
+  };
+
+  return (
+    <div className="h-full flex items-center justify-center bg-[#1b0918] ">
+      <Card className="md:h-auto w-[80%] sm:w-[420px] p-4 sm:p-8 ">
+        <CardHeader>
+          <CardTitle className="text-center">Sign Up</CardTitle>
+          <CardDescription className="text-sm text-center text-accent-foreground">
+            Use Email and Services to create account
+          </CardDescription>
+        </CardHeader>
+        {!!error && (
+          <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6 ">
+            <TriangleAlert />
+            <p>{error}</p>
+          </div>
+        )}
+        <CardContent className="px-2 sm:px-6 ">
+          <form onSubmit={handleSubmit} className="space-y-3">
+            <Input
+              type="text"
+              disabled={pending}
+              value={form.name}
+              onChange={(e) => setForm({ ...form, name: e.target.value })}
+              required
+              placeholder="Full Name"
+            />
+            <Input
+              type="email"
+              disabled={pending}
+              value={form.email}
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+              required
+              placeholder="Email"
+            />
+            <Input
+              type="password"
+              disabled={pending}
+              value={form.password}
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+              required
+              placeholder="Password"
+            />
+            <Input
+              type="password"
+              disabled={pending}
+              value={form.confirmPassword}
+              onChange={(e) =>
+                setForm({ ...form, confirmPassword: e.target.value })
+              }
+              required
+              placeholder="Confirm Password"
+            />
+            <Button
+              className="w-full"
+              size={"lg"}
+              variant="default"
+              type="submit"
+              disabled={pending}
+            >
+              Continue
+            </Button>
+            <Separator />
+            <div className="flex my-2 justify-evenly mx-auto items-center">
+              <Button
+                disabled={false}
+                onClick={() => {}}
+                variant="outline"
+                size="lg"
+                className="bg-slite-300 hover:bg-slate-400 hover:scale-110 "
+              >
+                <FcGoogle className="size-8 left-2.5 top-2.5" />
+              </Button>
+              <Button
+                disabled={false}
+                onClick={(e) => handleProvider(e, "github")}
+                variant="outline"
+                size="lg"
+                className="bg-slite-300 hover:bg-slate-400 hover:scale-110 "
+              >
+                <FaGithub className="size-8 left-2.5 top-2.5" />
+              </Button>
+            </div>
+            <p className="text-center text-sm mt-2 text-muted-foreground">
+              Already have an account?
+              <Link
+                href="sign-in"
+                className="text-sky-700 hover:underline  cursor-pointer"
+              >
+                {" "}
+                Sign In
+              </Link>
+            </p>
+          </form>
+        </CardContent>
+      </Card>
+      Sign Up
+    </div>
+  );
+};
+
+export default SignUp;
