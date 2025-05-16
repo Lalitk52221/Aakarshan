87def1d4135f893d878bdf8ae513f6b7301123f4
Lalit kumar <139781963+Lalitk52221@users.noreply.github.com>
2025-05-16 22:29:21 +0530

    added student list

--- src/app/(dashboard)/list/students/page.tsx
+++ src/app/(dashboard)/list/students/page.tsx
@@ -1,9 +1,124 @@
-import React from 'react'
+import Pagination from "@/components/ui/Pagination";
+import TableSearch from "@/components/ui/TableSearch";
+import Image from "next/image";
+import React from "react";
+import Table from "@/components/ui/Table";
+import Link from "next/link";
+import { role, studentsData, } from "@/lib/data";
 
+type Student = {
+  id: number;
+  studentId : string;
+  name: string;
+  email?: string;
+  photo?: string;
+  phone: string;
+  grade: string;
+  class: string;
+  address: string;
+};
+
+const columns = [
+  {
+    header: "Info",
+    accessor: "info",
+  },
+  {
+    header: "Student ID",
+    accessor: "studentId",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Grade",
+    accessor: "grade",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Class",
+    accessor: "class",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Phone",
+    accessor: "phone",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Address",
+    accessor: "address",
+    className: "hidden lg:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "actions",
+  },
+];
 const StudentList = () => {
-  return (
-    <div>StudentList</div>
-  )
-}
+  const renderRow = (item: Student) => (
+    <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight">
+      <td className="flex items-center gap-2 p-4">
+        <Image
+          src={item.photo || "/avatar.png"}
+          alt=""
+          width={40}
+          height={40}
+          className="md:hidden xl:block rounded-full h-10 w-10 object-cover"
+        />
+        <div className="flex flex-col">
+          <h3 className="font-semibold">{item.name}</h3>
+          <p className="text-xs text-gray-500">{item?.email}</p>
+        </div>
+        </td>
+        <td className="hidden md:table-cell">{item.studentId}</td>
+        <td className="hidden md:table-cell">{item.grade}</td>
+        <td className="hidden md:table-cell">{item.class}</td>
+        <td className="hidden lg:table-cell">{item.phone}</td>
+        <td className="hidden lg:table-cell">{item.address}</td>
+      
+      <td>
+        <div className="flex items-center gap-2 ">
+          <Link href={`/list/teachers/${item.id}`}>
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
+              <Image src="/view.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-gray-300">
+              <Image src="/delete.png" alt="" width={16} height={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
 
-export default StudentList
+  return (
+    <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
+      {/* top section  */}
+      <div className="flex justify-between">
+        <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
+        <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
+          <TableSearch />
+          <div className="flex items-center gap-3 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/filter.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/sort.png" alt="" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
+              <Image src="/plus.png" alt="" width={14} height={14} />
+            </button>
+          </div>
+        </div>
+      </div>
+      {/* LIST  */}
+      <Table columns={columns} renderRow = {renderRow} data={studentsData} />
+      <div className=""></div>
+      {/* PAGINATION  */}
+      <Pagination />
+    </div>
+  );
+};
+
+export default StudentList;
